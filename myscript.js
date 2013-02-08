523bf93215efb87c2d36246b071757ba89091c30
Neil Everette <neil.everette@gmail.com>
2013-02-08 08:52:42 -0800

    Added real images
    Addition of images to the slideshow, changed png to jpg, created
    function to change image

--- myscript.js
+++ myscript.js
@@ -1,6 +1,6 @@
 window.onload = initAll;
 
-var theCount = 0;
+var counter = 1;
 
 myCounter = document.getElementById("myCounter");
 mainImage = document.getElementById("mainImage");
@@ -10,31 +10,34 @@ function initAll() {
     document.getElementById("next").onclick = addOne;
     document.getElementById("prev").onclick = minusOne;
 
-    mainImage.src = "images/image_" + theCount + ".png";
-
-    console.log(theCount);
+    setImage(counter);
+	console.log(counter);
 }
 
 function addOne() {
 
-    if (theCount == 4) { //checks to see if the count exceeds the number of images
-        theCount = 0; //if the count exceeds lenght, then it resets the count to 0
+    if (counter > 25) { //checks to see if the count exceeds the number of images
+        counter = 1; //if the count exceeds lenght, then it resets the count to 0
     } else {
-        theCount++;
+        counter++;
     }
-    myCounter.innerHTML = "The count is " + theCount; // sets the correct PNG image
-    mainImage.src = "images/image_" + theCount + ".png"; // sets the correct image to the count
+    myCounter.innerHTML = "The count is " + counter; // sets the correct PNG image
+    setImage(counter);
     return false;
 
 }
 
 function minusOne() {
-    if (theCount == 0) {
-        theCount = 4;
+    if (counter == 0) {
+        counter = 25;
     } else {
-        theCount--;
+        counter--;
     }
-    myCounter.innerHTML = "The count is " + theCount;
-    mainImage.src = "images/image_" + theCount + ".png"; //sets the correct image to the count
+    myCounter.innerHTML = "The count is " + counter;
+    setImage(counter);
     return false;
+}
+
+function setImage(x){
+    mainImage.src = "images/" + counter + ".jpg"; //sets the correct image to the count
 }
